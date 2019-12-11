f5e492447871b39e20f4c429614372ed1ef1e4f2
Naveed <sr.naveed99@gmail.com>
2019-12-12 02:11:11 +0500

    - adapter added

---
+++ app/src/main/java/com/payback/AppExecutors.kt
@@ -0,0 +1,39 @@
+package com.payback
+
+import java.util.concurrent.Executor
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Global executor pools for the whole application.
+ *
+ * Grouping tasks like this avoids the effects of task starvation (e.g. disk reads don't wait behind
+ * webservice requests).
+ */
+@Singleton
+open class AppExecutors(
+    private val diskIO: Executor,
+    private val mainThread: Executor
+) {
+
+    @Inject
+    constructor() : this(
+        Executors.newSingleThreadExecutor(),
+        MainThreadExecutor()
+    )
+
+    fun diskIO(): Executor {
+        return diskIO
+    }
+
+    fun mainThread(): Executor {
+        return mainThread
+    }
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+}
