6295ca20b25c801ad8d1785d550f66aef8a0f6ff
Naveed <sr.naveed99@gmail.com>
2019-12-13 10:43:15 +0500

    Added ui tests

---
+++ app/src/androidTest/java/com/payback/ui/MainActivityTest.kt
@@ -0,0 +1,93 @@
+package com.payback.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.payback.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val cardView = onView(
+            allOf(
+                childAtPosition(
+                    allOf(
+                        withId(R.id.photo_list_recycler_view),
+                        childAtPosition(
+                            withClassName(`is`("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        Thread.sleep(8000)
+        cardView.perform(click())
+
+        Thread.sleep(3000)
+        pressBack()
+
+        val cardView2 = onView(
+            allOf(
+                childAtPosition(
+                    allOf(
+                        withId(R.id.photo_list_recycler_view),
+                        childAtPosition(
+                            withClassName(`is`("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                            1
+                        )
+                    ),
+                    5
+                ),
+                isDisplayed()
+            )
+        )
+        Thread.sleep(2000)
+        cardView2.perform(click())
+
+        Thread.sleep(2000)
+        pressBack()
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
